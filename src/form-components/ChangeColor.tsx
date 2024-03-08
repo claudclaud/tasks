428830cd2cb019286e72aa369899318d0676c642
claudia chance <cchance@udel.edu>
2024-03-07 22:11:48 -0500

    Finished yay

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,50 @@
 import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+
+const colors = [
+    "red",
+    "blue",
+    "green",
+    "yellow",
+    "orange",
+    "purple",
+    "pink",
+    "cyan"
+];
 
 export function ChangeColor(): JSX.Element {
+    const [selectedColor, setSelectedColor] = useState<string>(colors[0]);
+
+    const handleColorChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSelectedColor(event.target.value);
+    };
+
     return (
         <div>
-            <h3>Change Color</h3>
+            <Form>
+                {colors.map((color, index) => (
+                    <Form.Check
+                        key={index}
+                        inline
+                        type="radio"
+                        label={color}
+                        value={color}
+                        checked={selectedColor === color}
+                        onChange={handleColorChange}
+                    />
+                ))}
+            </Form>
+            <div
+                data-testid="colored-box"
+                style={{
+                    backgroundColor: selectedColor,
+                    width: "100px",
+                    height: "100px",
+                    marginTop: "10px"
+                }}
+            >
+                {selectedColor}
+            </div>
         </div>
     );
 }
--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,34 @@
 import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [userAnswer, setUserAnswer] = useState<string>("");
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setUserAnswer(event.target.value);
+    };
+
     return (
-        <div>
-            <h3>Check Answer</h3>
-        </div>
+        <Form>
+            <Form.Group controlId="userAnswer">
+                <Form.Label>Your Answer:</Form.Label>
+                <Form.Control
+                    type="text"
+                    value={userAnswer}
+                    onChange={handleChange}
+                />
+            </Form.Group>
+            <div>
+                {userAnswer === expectedAnswer ? (
+                    <span style={{ color: "green" }}>✔️ Correct!</span>
+                ) : (
+                    <span style={{ color: "red" }}>❌ Incorrect</span>
+                )}
+            </div>
+        </Form>
     );
 }
